81ba33d0d4c4da7ce9ee4da80d1bec65f07a0fa6
lendale <nasibag1>
2016-05-10 20:03:33 +0800

    *Added LoginActivity with validate method *Modified colors.xml *Added custom theme in styles.xml *Added activity_login and content_login layouts for LoginActivity

---
+++ app/src/main/java/com/acg/equipt/LoginActivity.java
@@ -0,0 +1,120 @@
+package com.acg.equipt;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.AppCompatButton;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class LoginActivity extends AppCompatActivity {
+    private static final int REQUEST_SIGNUP = 0;
+    @BindView(R.id.et_email)
+    EditText etEmail;
+    @BindView(R.id.et_passwd)
+    EditText etPasswd;
+    @BindView(R.id.btn_login)
+    AppCompatButton btnLogin;
+    @BindView(R.id.coordinatorLayout)
+    CoordinatorLayout coordinatorLayout;
+    private String TAG = LoginActivity.class.getSimpleName();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        ButterKnife.bind(this);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                login();
+            }
+        });
+    }
+
+    public void login() {
+        if (!validate()) {
+            onLoginFailed();
+            return;
+        }
+
+        btnLogin.setEnabled(false);
+
+        final ProgressDialog progressDialog = new ProgressDialog(LoginActivity.this,
+                R.style.AppTheme_Dark_Dialog);
+        progressDialog.setIndeterminate(true);
+        progressDialog.setMessage("Authenticating...");
+        progressDialog.show();
+
+        new android.os.Handler().postDelayed(
+                new Runnable() {
+                    public void run() {
+                        // On complete call either onLoginSuccess or onLoginFailed
+                        onLoginSuccess();
+                        // onLoginFailed();
+                        progressDialog.dismiss();
+                    }
+                }, 2000);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_SIGNUP) {
+            if (resultCode == RESULT_OK) {
+
+                // TODO: Implement successful signup logic here
+                // By default we just finish the Activity and log them in automatically
+                this.finish();
+            }
+        }
+    }
+
+    @Override
+    public void onBackPressed() {
+        // Disable going back to the MainActivity
+        moveTaskToBack(true);
+    }
+
+    public void onLoginSuccess() {
+        btnLogin.setEnabled(true);
+        finish();
+    }
+
+    public void onLoginFailed() {
+        Toast.makeText(getBaseContext(), "Login failed", Toast.LENGTH_LONG).show();
+
+        btnLogin.setEnabled(true);
+    }
+
+    public boolean validate() {
+        boolean valid = true;
+
+        String email = String.valueOf(etEmail.getText());
+        String passwd = String.valueOf(etPasswd.getText());
+
+        if (email.isEmpty() || !android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            etEmail.setError("Enter a valid email address");
+            valid = false;
+        } else {
+            etEmail.setError(null);
+        }
+
+        if (passwd.isEmpty() || passwd.length() < 4 || passwd.length() > 10) {
+            etPasswd.setError("between 4 and 10 alphanumeric characters");
+            valid = false;
+        } else {
+            etPasswd.setError(null);
+        }
+
+        return valid;
+    }
+
+}
