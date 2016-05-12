af20c54fb9a4731f1742c7131a107b649be232b0
lendale <nasibag1>
2016-05-12 13:13:19 +0800

    *Added MapsActivity *Modified fab to start mapIntent when clicked

--- app/src/main/java/com/acg/equipt/MainActivity.java
+++ app/src/main/java/com/acg/equipt/MainActivity.java
@@ -1,13 +1,20 @@
 package com.acg.equipt;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.net.Uri;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+    private String LOG_TAG = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +25,17 @@ public class MainActivity extends AppCompatActivity {
 
         Intent loginIntent = new Intent(this, LoginActivity.class);
         startActivity(loginIntent);
+
+        final Intent mapIntent = new Intent(this, MapsActivity.class);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+//                openGoogleMap();
+                startActivity(mapIntent);
+            }
+        });
     }
 
     @Override
@@ -40,5 +58,27 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private void openGoogleMap() {
+        SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(this);
+        String location = sharedPrefs.getString("location", "Cebu");
+
+        // Using the URI scheme for showing a location found on a map.  This super-handy
+        // intent can is detailed in the "Common Intents" page of Android's developer site:
+        // http://developer.android.com/guide/components/intents-common.html#Maps
+        Uri geoLocation = Uri.parse("geo:0,0?").buildUpon()
+                .appendQueryParameter("q", location)
+                .build();
+
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.setData(geoLocation);
+
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        } else {
+            Log.d(LOG_TAG, "Couldn't call " + location + ", no receiving apps installed!");
+        }
+
     }
 }
