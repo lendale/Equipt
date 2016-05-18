82d8a310c275cb4b74cf4cab9a4193c0f015ca95
lendale <nasibag1>
2016-05-18 10:41:52 +0800

    *Added Firebase Gradle Dependency *Implemented Firebase initialization

--- app/src/main/java/com/acg/equipt/LoginActivity.java
+++ app/src/main/java/com/acg/equipt/LoginActivity.java
@@ -33,6 +33,8 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        Firebase.setAndroidContext(this);
+
         ButterKnife.bind(this);
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
